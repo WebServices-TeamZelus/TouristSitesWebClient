87c1eb204fb63925507e86426975aa7ed0909029
Nikolay Atanasow <n.atanasow@yahoo.com>
2015-11-18 12:16:31 +0200

    Simple chat added.

---
+++ js/chat.js
@@ -0,0 +1,45 @@
+var menuRight = $('#cbp-spmenu-s2'),
+    showChat = $('#btn-chat');
+
+
+showChat.on('click', function () {
+    showChat.toggleClass('active');
+    menuRight.toggleClass('cbp-spmenu-open');
+
+// for the chat
+    var sendButton = $('#btn-send');
+    var inputField = $('#chat-input');
+    var messageContainer = $('#msg-container');
+
+    var pubnub = PUBNUB({
+        subscribe_key: 'demo', // always required
+        publish_key: 'demo'    // only required if publishing
+    });
+
+    sendButton.on('click', function () {
+        var messageValue = inputField.val();
+        inputField.val('');
+        pubnub.publish({
+            channel: 'my_channel',
+            message: messageValue,
+            callback: function (m) { }
+        });
+    });
+
+    pubnub.subscribe({
+        channel: 'my_channel',
+        message: function (m) {
+            appendMessage(m);
+        }
+    });
+
+    function appendMessage(message) {
+        messageContainer.append(message + '\n');
+    }
+
+    inputField.keyup(function (event) {
+        if (event.keyCode == 13) {
+            sendButton.click();
+        }
+    });
+});
