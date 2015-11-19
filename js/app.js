1d1d636e4377e71747d7ef1a0b24598b6bec625c
iliantova <iliyana.antova@gmail.com>
2015-11-19 13:49:33 +0200

    upload image

--- js/app.js
+++ js/app.js
@@ -79,46 +79,47 @@
     function touristSiteTemplate(id) {
 
         data.touristSites.getById(id).then(function(res) {
-            console.log(id);
             touristObj = res;
-            console.log(res);
-            //images = touristObj.Images;
-            return templates.get('touristSiteTemplate');
+          return templates.get('touristSiteHeader');
         })
             .then(function(template) {
-                $('.container').html(template(touristObj));
+                $('#headerTemplates').html(template(touristObj));
                 var img = images[0].Url;
-                $('.codrops-header').css('background-image', '../' + 'img');
+               $('.codrops-header').css('background-image', "\'url("+ img +")\'");
+                return templates.get('touristSiteDiscriptions');
+            })
+            .then(function(template) {
+                $('#discriptionTemplates').html(template(touristObj));
                 return templates.get('galery');
             })
             .then(function(template) {
-                $('#photostack-1').html(template(images));
+                var imageTouristsite  = images.filter(function(i){return i.TouristSiteName == touristObj.Name;} );
+                console.log(imageTouristsite);
+                $('#photostack-1').html(template(imageTouristsite));
                 [].slice.call( document.querySelectorAll( '.photostack' ) ).forEach( function( el ) { new Photostack( el ); } );
                 new Photostack( document.getElementById( 'photostack-1' ), {
                     callback : function( item ) {
                     }
                 } );
-            });
+            })
+    .then(function() {
+               // if (localStorage.getItem(LOCAL_STORAGE_USERNAME_KEY) != undefined){
+                    $('#imageLoad').css('display', 'block');
 
-        if (localStorage.getItem(LOCAL_STORAGE_USERNAME_KEY) == undefined) {
-
-            $('#imageLoad').css('display', 'none');
-
-        }
-        else{
-
-            $('#submit-image').on('click', function (ev) {
-                var url = $('#url-image').val();
-                console.log(url);
-                data.images.addImage(url, touristObj.Id, LOCAL_STORAGE_AUTHKEY_KEY)
-                    .then(function (res) {
-                        toastr.success('Successfully upload picture');
-                    },function (rej) {
-                        toastr.error('Failed upload image');
-                        console.log('register faild ' + rej['Message']);
+                    $('#submit-image').on('click', function (ev) {
+                        var url = $('#url-image').val();
+                        console.log(url);
+                        data.images.addImage(url, touristObj.Id, LOCAL_STORAGE_AUTHKEY_KEY)
+                            .then(function (res) {
+                                toastr.success('Successfully upload picture');
+                            },function (rej) {
+                                toastr.error('Failed upload image');
+                                console.log('register faild ' + rej['Message']);
+                            });
                     });
-            });
-        }
+               // }
+        });
+
     }
 
     $('.test').on('click', function (ev) {
