7b1e802d4db55660e868115a1b5644d3e8434c29
Nikolay Atanasow <n.atanasow@yahoo.com>
2015-11-19 17:26:23 +0200

    Logic from app.js moved in homeController.js and sidesController.js

---
+++ js/homeController.js
@@ -0,0 +1,85 @@
+var homeController = (function () {
+
+    function getHomePage() {
+        var images,
+            LOCAL_STORAGE_USERNAME_KEY = 'signed-in-user-username',
+            LOCAL_STORAGE_AUTHKEY_KEY = 'signed-in-user-auth-key';
+
+        localStorage.clear();
+
+        var touristObj;
+
+        var loginButton = $('#btn-login'),
+            mapButton = $('#btn-map');
+
+        mapButton.on('click', function () {
+            $('svg').first().hide();
+            $('h1').first().hide();
+            $('#header-id').removeClass('codrops-header').addClass('map');
+            $(this).hide();
+        });
+
+        data.images.getAll().then(function (res) {
+            images = res;
+            return templates.get('galery');
+        })
+            .then(function (template) {
+                $('#photostack-1').html(template(images));
+                [].slice.call(document.querySelectorAll('.photostack')).forEach(function (el) { new Photostack(el); });
+                new Photostack(document.getElementById('photostack-1'), {
+                    callback: function (item) {
+                    }
+                });
+            });
+
+        loginButton.on('click', function () {
+            if (localStorage.getItem(LOCAL_STORAGE_USERNAME_KEY) != undefined) {
+                localStorage.removeItem(LOCAL_STORAGE_USERNAME_KEY);
+                localStorage.clear(LOCAL_STORAGE_AUTHKEY_KEY);
+                $('#greetingDiv').remove();
+                toastr.success('Successfully logged out');
+            }
+
+            else {
+
+                var template = templates.get('loginTemplate');
+                var formContainer = $('.loginForm');
+                formContainer.html(template(''));
+                $('#loginForm').submit(function (ev) {
+                    ev.preventDefault();
+                    var email = $('#emailInput').val();
+                    var pass = $('#passInput').val();
+                    data.users.login(email, pass)
+                        .then(function (res) {
+                            localStorage.setItem(LOCAL_STORAGE_USERNAME_KEY, email);
+                            localStorage.setItem(LOCAL_STORAGE_AUTHKEY_KEY, res['access_token']);
+                            toastr.success('Successfully logged in');
+                            loginButton.parent().append('<div id="greetingDiv">Hello ' + email + '</div>');
+                            formContainer.html('');
+                        }, function (rej) {
+                            toastr.error('Invalid Username or Passoword');
+                            console.log('register faild ' + rej['Message']);
+                        });
+                });
+
+                $('#register').on('click', function (ev) {
+                    var email = $('#emailInput').val();
+                    var pass = $('#passInput').val();
+                    data.users.register(email, pass)
+                        .then(function (res) {
+                            toastr.success('Successfully logged in');
+                            formContainer.html('');
+                        }, function (rej) {
+                            toastr.error('Invalid Username or Passoword');
+                            console.log('register faild ' + rej['Message']);
+                        });
+                });
+            }
+        });
+
+    }
+
+    return {
+        getHomePage: getHomePage
+    }
+} ());
---
+++ js/sidesController.js
@@ -0,0 +1,51 @@
+var sidesController = (function touristSiteTemplate() {
+
+    function getCurrentSide(id) {
+        data.touristSites.getById(id).then(function (res) {
+            touristObj = res;
+            return templates.get('touristSiteHeader');
+        })
+            .then(function (template) {
+                $('#headerTemplates').html(template(touristObj));
+                var img = images[0].Url;
+                $('.codrops-header').css('background-image', "\'url(" + img + ")\'");
+                return templates.get('touristSiteDiscriptions');
+            })
+            .then(function (template) {
+                $('#discriptionTemplates').html(template(touristObj));
+                return templates.get('galery');
+            })
+            .then(function (template) {
+                var imageTouristsite = images.filter(function (i) { return i.TouristSiteName == touristObj.Name; });
+                console.log(imageTouristsite);
+                $('#photostack-1').html(template(imageTouristsite));
+                [].slice.call(document.querySelectorAll('.photostack')).forEach(function (el) { new Photostack(el); });
+                new Photostack(document.getElementById('photostack-1'), {
+                    callback: function (item) {
+                    }
+                });
+            })
+            .then(function () {
+                // if (localStorage.getItem(LOCAL_STORAGE_USERNAME_KEY) != undefined){
+                $('#imageLoad').css('display', 'block');
+
+                $('#submit-image').on('click', function (ev) {
+                    var url = $('#url-image').val();
+                    console.log(url);
+                    data.images.addImage(url, touristObj.Id, LOCAL_STORAGE_AUTHKEY_KEY)
+                        .then(function (res) {
+                            toastr.success('Successfully upload picture');
+                        }, function (rej) {
+                            toastr.error('Failed upload image');
+                            console.log('register faild ' + rej['Message']);
+                        });
+                });
+
+            });
+    }
+
+    return {
+        getCurrentSide: getCurrentSide
+    }
+
+} ());
